20f484b4f5a4a0b638153dd4cdd11ba02c71f841
suhJh <sjs0701@me.com>
2016-11-04 18:49:47 +0900

    DESIGNING HTML TEMPLATE FOR APP
    header body chat speech bubble etc...
    
    not bad. but look like so amature..

--- gulp/gulp-config.js
+++ gulp/gulp-config.js
@@ -1,11 +1,11 @@
 const src = 'src/main/webapp';
 
 module.exports = {
-    src: {
-        js: `${src}/app`,
-        css: `${src}/css`,
-        html: `${src}/html-design`,
-        img: `${src}/img`,
-        bundle: src,
-    },
+  src: {
+    js: `${src}/app`,
+    css: `${src}/css`,
+    html: `${src}/html-layout`,
+    img: `${src}/images`,
+    bundle: src,
+  },
 };
--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const config = require('./gulp/gulp-config');
 
 const babelify = require('babelify');
 const browserify = require('browserify');
-const watchify = require('watchify');
+//  const watchify = require('watchify');
 
 const source = require('vinyl-source-stream'); // ->얘를 써야하는 이유 http://programmingsummaries.tistory.com/382
-const buffer = require("vinyl-buffer");
-const merge = require('utils-merge');
-const sourcemaps = require('gulp-sourcemaps')
-const uglify = require("gulp-uglify");
-const rename = require('gulp-rename')
+//  const buffer = require("vinyl-buffer");
+//  const merge = require('utils-merge');
+//  const sourcemaps = require('gulp-sourcemaps')
+//  const uglify = require("gulp-uglify");
+//  const rename = require('gulp-rename')
 
 /* 오류 잡이 */
 const handleErrors = require('./gulp/handleErrors');
@@ -19,41 +19,39 @@ const notify = require('gulp-notify'); // windows alert
 
 const browserSync = require('browser-sync');
 
-
-
 gulp.task('default', [
-    'browser-sync',
-    'build',
-    'watch',
+  'browser-sync',
+  'build',
+  'watch',
 ]);
 
 
 gulp.task('browser-sync', () => {
-        browserSync.init(null, {
-        proxy: 'http://localhost:8080',
-        files: [`${config.src.js}/*.*`],
-        port: 3000,
-        open: 'external',
-    });
+  browserSync.init(null, {
+    proxy: 'http://localhost:8080',
+    files: [`${config.src.js}/*.*`],
+    port: 3000,
+    open: 'external',
+  });
 });
 
 
 gulp.task('build', () => {
-    const optionalB = browserify({
-        insertGlobals: true,
-        extensions: ['.js', '.jsx'],
-    });
+  const optionalB = browserify({
+    insertGlobals: true,
+    extensions: ['.js', '.jsx'],
+  });
 
-    optionalB.transform(babelify, { presets: ['react', 'es2015'] });
+  optionalB.transform(babelify, { presets: ['react', 'es2015'] });
 
-    try { //  build에 실패하면 실패하기 전의 소스를 스트림으로 반환
-        optionalB.add(`${config.src.js}/index.jsx`);
-    } catch (err) {
-        notify.write(err);
-        console.log(err);
-    }
+  try { //  build에 실패하면 실패하기 전의 소스를 스트림으로 반환
+    optionalB.add(`${config.src.js}/index.jsx`);
+  } catch (err) {
+    notify.write(err);
+    console.log(err);
+  }
 
-    return optionalB.bundle()
+  return optionalB.bundle()
         .on('error', handleErrors)
         .pipe(source('bundle.js'))
         //  .pipe(buffer())//->ugilfy도 vinyl스트림을 사용하므로 버퍼가 필요
@@ -64,7 +62,8 @@ gulp.task('build', () => {
 
 
 gulp.task('watch', () => {
-    gulp.watch(config.src.img + '/**/*.jpg').on('change', browserSync.reload);
-    gulp.watch(config.src.html + '/**/*.html').on('change', browserSync.reload);
-    gulp.watch(`${config.src.js}/**/*.*`, ['build']);
+  gulp.watch(`${config.src.img}/**/*.jpg`).on('change', browserSync.reload);
+  gulp.watch(`${config.src.html}/**/*.html`).on('change', browserSync.reload);
+  gulp.watch(`${config.src.css}/**/*.css`).on('change', browserSync.reload);
+  gulp.watch(`${config.src.js}/**/*.*`, ['build']);
 });
